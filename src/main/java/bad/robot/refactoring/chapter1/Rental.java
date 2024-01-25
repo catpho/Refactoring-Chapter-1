a1eff48dc62d0e5126224a7ec81fa8043cc8600a
cathyphan <cathyp98@outlook.com>
2024-01-24 19:55:50 -0500

    buggy

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -41,4 +41,11 @@ public class Rental {
 		return amount;
 	}
 
+	int calculateFrequentRenterPoints() {
+		
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+		    return 2;
+		return 1;
+	}
+
 }
